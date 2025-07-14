078959fa85d5fe0c774ddf687e25bb245b66cbd8
rodrigojp2005 <rodrigojp2005@gmail.com>
2025-07-14 15:38:15 -0300

    Muitas modificaçoes: foi instalado o breeze auth, agora mais de 60 commit serão feito, dentre eles os marges em alguns arquivos que stavam funcionando (dasboard, web, welcome). A partir deste ponto, deve ser adaptado views, controolers e models anteriores a arquitetura atual.

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GameController;
-use App\Http\Controllers\GincanaController;
 
-Route::get('/', [GameController::class, 'index']);
-
-// Rotas para gincanas
-Route::get('/gincana/criar', [GincanaController::class, 'criar'])->name('gincana.criar');
-Route::post('/gincana', [GincanaController::class, 'store'])->name('gincana.store');
-Route::get('/gincana', [GincanaController::class, 'index'])->name('gincana.index');
-
-// Novas rotas para "Minhas Gincanas"
-Route::get('/minhas-gincanas', [GincanaController::class, 'minhasGincanas'])->name('gincana.minhas');
-Route::get('/gincana/{id}/editar', [GincanaController::class, 'edit'])->name('gincana.edit');
-Route::put('/gincana/{id}', [GincanaController::class, 'update'])->name('gincana.update');
-Route::delete('/gincana/{id}', [GincanaController::class, 'destroy'])->name('gincana.destroy');
-
-// Rota para gincanas participadas
-Route::get('/gincanas-participadas', [GincanaController::class, 'gincanasParticipadas'])->name('gincana.participadas');
-
-// Rotas para carregar gincanas (API)
-Route::get("/gincana/new", [GincanaController::class, "newGincana"]);
-Route::get("/gincana/skip", [GincanaController::class, "skipGincana"]);
-Route::get("/gincana/{id}", [GincanaController::class, "getGincana"]);
-Route::get("/api/gincanas/public", [GincanaController::class, "getPublicGincanas"]);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+require __DIR__.'/auth.php';
